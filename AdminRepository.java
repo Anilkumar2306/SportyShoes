865fa06e39cb9c9aabd6f6958c57d76ca71ac66c
Anilkumar2306 <102856371+Anilkumar2306@users.noreply.github.com>
2022-05-13 14:25:41 +0530

    Add files via upload

---
+++ AdminRepository.java
@@ -0,0 +1,10 @@
+
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.sportshoes.entity.AdminDetails;
+
+public interface AdminRepository extends CrudRepository<AdminDetails, Integer> {
+
+	AdminDetails findByuserName(Object object);
+}
